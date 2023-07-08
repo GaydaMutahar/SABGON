25fa680a047314858d420df1015dc24a4e87b3d2
GaydaMutahar <38920790+GaydaMutahar@users.noreply.github.com>
2023-07-08 17:24:29 +0300

    Add files via upload

--- medAgent2.py
+++ medAgent2.py
@@ -10,27 +10,26 @@ from langchain.prompts import StringPromptTemplate
 from langchain.tools import DuckDuckGoSearchRun 
 
 from typing import List, Union
-#from langchain.schema import AgentAction, AgentFinish
 import re
 import langchain
-
-
+ 
 #st.set_page_config(layout="wide") 
 col1, col2, col3 = st.columns(3 , gap="large")
-with col1:
-    st.title(":blue[Medical  Advisor ]")
-with col2:
-    image = Image.open('im.jpeg')
-    st.image(
+#with col1:
+ #   st.title(":blue[MedSmart]")
+#with col2:
+image = Image.open('im2.jpeg')
+st.image(
         image,
-        width=120,
+        width=300,
     )
 
 co = cohere.Client('GGM7uhPK5DTgPtMweS0IbHK2rkxLBmAJ2cmoGA57')
 prompt1 = st.text_input('What are the symptoms of the patient?  (*Please type the correct spelling of the symptom**)')
+but1 = st.button('Find a medicine')
 with open('symptoms_list.txt', 'r') as file:
     symptoms = [line.strip().lower() for line in file]
-    print("symptoms", symptoms)
+    
 if prompt1:
     if any(symptom in prompt1.lower() for symptom in symptoms):
         response = co.generate(
@@ -47,11 +46,12 @@ if prompt1:
         )
 
         text = format(response.generations[0].text)
-        if st.button('Find a medicine'):
+        if but1:
              st.write('Prescription medications: %s' %text)
 
 
 prompt2 = st.text_input('What are the heatlh conditions that conflict with this medicine ? (*Please type the correct spelling of the medicine**) ')
+#but2 = st.button('List health conditions')
 if prompt2:
                      response = co.generate(
                             model = 'command-nightly', 
@@ -66,9 +66,9 @@ if prompt2:
                             return_likelihoods='NONE'
                         )
                      text2 = format(response.generations[0].text)
+                     
                      st.write('List of health conditions: %s' %text2)
                      print("Seconed prompt is done")
-
 
 prompt3 = st.text_input('Ask me, if you have any general heatlh questions:')
 
